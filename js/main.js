89532e01e13d75290b17cad2bd7a22371162a717
namnguyenthanhwork <78526193+namnguyenthanhwork@users.noreply.github.com>
2021-11-03 22:09:35 +0700

    update + reponsive

--- js/main.js
+++ js/main.js
@@ -14,7 +14,14 @@ $(function () {
             $(".navbar").addClass("sticky");
         }
     });
-
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function (event) {
+        if ($(this).scrollTop() > 600) {
+            $('.back-to-top').fadeIn(200)
+        } else {
+            $('.back-to-top').fadeOut(200)
+        }
+    });
 })
 // init cursor
 var cursors = [{
