beba1da4ae1964636d5f673b4e0159f1b1e73439
namnguyenthanhwork <78526193+namnguyenthanhwork@users.noreply.github.com>
2021-11-06 23:45:11 +0700

    update info

--- js/main.js
+++ js/main.js
@@ -53,7 +53,143 @@ $(function () {
             }
         }
     })
+
+
 })
+
+
+let faculty = [{
+        name: 'Khoa Học Máy Tính',
+        image: './assets/images/faculty/laptop.svg',
+        link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/Chuan%20dau%20ra%20KHMT%20060919.pdf'
+    },
+    {
+        name: 'Công Nghệ Kỹ Thuật Công Trình Xây Dựng',
+        image: './assets/images/faculty/contruction.png',
+        link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/C%C4%90R%20ng%C3%A0nh%20X%C3%A2y%20D%E1%BB%B1ng.pdf'
+    },
+    {
+        name: 'Quản Lý Xây Dựng',
+        image: './assets/images/faculty/worker.png',
+        link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/C%C4%90R%20%20QLXD-CDR%20nop.pdf'
+    },
+    {
+        name: 'Công Nghệ Sinh Học',
+        image: './assets/images/faculty/biotechnology.png',
+        link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/C%C4%90R%20C%C3%B4ng%20ngh%E1%BB%87%20sinh%20h%E1%BB%8Dc.pdf'
+    },
+    {
+        name: 'Hệ Thống Thông Tin Quản Lý',
+        image: '',
+        link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/chuan%20dau%20ra%20HTTTQL%20150819.pdf'
+    },
+    {
+        name: 'Công Nghệ Thông Tin',
+        image: '',
+        link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/Chuan%20dau%20ra%20KHMT%20060919.pdf'
+    },
+    {
+        name: 'Quản Trị Kinh Doanh',
+        image: '',
+        link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/C%C4%90R%20QTKD%20-%20C%C4%90R.pdf'
+    },
+    {
+        name: 'Kinh Doanh Quốc Tế',
+        image: '',
+        link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/CT%C4%90T%20KDQT.pdf'
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    },
+    {
+        name: '',
+        image: '',
+        link: ''
+    }
+]
+
+let faculty_list = document.querySelector('#faculty')
+
+renderFaculty = (faculty) => {
+    faculty.forEach(e => {
+        let prod = `
+    <div class="col-4 md-4 s-6 xs-10 card">
+        <div class="card-inner">
+            <div class="card-icon">
+                <img src="${e.image}" alt="${e.name}" class="img-fluid">
+            </div>
+            <p><a href="${e.link}"
+                    class="hover-link">${e.name}</a></p>
+        </div>
+    </div>
+    `
+        faculty_list.insertAdjacentHTML("beforeend", prod)
+    })
+}
+renderFaculty(faculty)
+
 // init cursor
 var cursors = [{
     cursor_id: "3",
