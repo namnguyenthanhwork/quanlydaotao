d7da4d7fdf9f3b7197bc7c14d68c48ac357c4927
namnguyenthanhwork <78526193+namnguyenthanhwork@users.noreply.github.com>
2021-11-07 11:06:32 +0700

    update

--- js/faculty.js
+++ js/faculty.js
@@ -20,92 +20,92 @@ let faculty = [{
     },
     {
         name: 'Hệ Thống Thông Tin Quản Lý',
-        image: '../assets/images/faculty/information.png',
+        image: './assets/images/faculty/information.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/chuan%20dau%20ra%20HTTTQL%20150819.pdf'
     },
     {
         name: 'Công Nghệ Thông Tin',
-        image: '../assets/images/faculty/gear.png',
+        image: './assets/images/faculty/gear.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/Chuan%20dau%20ra%20KHMT%20060919.pdf'
     },
     {
         name: 'Quản Trị Kinh Doanh',
-        image: '../assets/images/faculty/accounting.png',
+        image: './assets/images/faculty/accounting.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/C%C4%90R%20QTKD%20-%20C%C4%90R.pdf'
     },
     {
         name: 'Kinh Doanh Quốc Tế',
-        image: '../assets/images/faculty/opportunity.png',
+        image: './assets/images/faculty/opportunity.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/CT%C4%90T%20KDQT.pdf'
     },
     {
         name: 'Marketing',
-        image: '../assets/images/faculty/protest.png',
+        image: './assets/images/faculty/protest.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/C%C4%90R%20Marketing%20-%20CDR.pdf'
     },
     {
         name: 'Quản Trị Nhân Lực',
-        image: '../assets/images/faculty/hr.png',
+        image: './assets/images/faculty/hr.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/C%C4%90R%20QTNL.pdf'
     },
     {
         name: 'Tài Chính Ngân Hàng',
-        image: '../assets/images/faculty/money.png',
+        image: './assets/images/faculty/money.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/CDR%20TCNH.pdf'
     },
     {
         name: 'Công Tác Xã Hội',
-        image: '../assets/images/faculty/help.png',
+        image: './assets/images/faculty/help.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/C%C4%90R.%20CTXH.pdf'
     },
     {
         name: 'Đông Nam Á Học',
-        image: '../assets/images/faculty/asia.png',
+        image: './assets/images/faculty/asia.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/C%C4%90R%20DNA.pdf'
     },
     {
         name: 'Xã Hội Học',
-        image: '../assets/images/faculty/link.png',
+        image: './assets/images/faculty/link.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/C%C4%90R%20XHH.pdf'
     },
     {
         name: 'Kinh Tế',
-        image: '../assets/images/faculty/economics.png',
+        image: './assets/images/faculty/economics.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/CDR_DH_Kinhte.pdf'
     },
     {
         name: 'Kế Toán',
-        image: '../assets/images/faculty/accounting201.png',
+        image: './assets/images/faculty/accounting201.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/CHUAN%20DAU%20RA-NGANH%20KETOAN.pdf'
     },
     {
         name: 'Kiểm Toán',
-        image: '../assets/images/faculty/audit.png',
+        image: './assets/images/faculty/audit.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/CDR%20NGANH%20KIEMTOAN_V4.pdf'
     },
     {
         name: 'Luật',
-        image: '../assets/images/faculty/law.png',
+        image: './assets/images/faculty/law.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/CDR%20nganh%20Luat_9.12.2019.pdf'
     },
     {
         name: 'Luật Kinh Tế',
-        image: '../assets/images/faculty/auction.png',
+        image: './assets/images/faculty/auction.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/CDR%20nganh%20Luat%20Kinh%20te_9.12.2019.pdf'
     },
     {
         name: 'Ngôn Ngữ Anh',
-        image: '../assets/images/faculty/uk.png',
+        image: './assets/images/faculty/uk.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/Chuan%20dau%20ra%20-%20NN%20Anh.pdf'
     },
     {
         name: 'Ngôn Ngữ Nhật',
-        image: '../assets/images/faculty/japan201.png',
+        image: './assets/images/faculty/japan201.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/CDR%20-%20NN%20Nhat.pdf'
     },
     {
         name: 'Ngôn Ngữ Trung Quốc',
-        image: '../assets/images/faculty/china.png',
+        image: './assets/images/faculty/china.png',
         link: 'http://quanlydaotao.ou.edu.vn/media/files/ChuanDauRa/CDR%20NNTQ.pdf'
     }
 ]
